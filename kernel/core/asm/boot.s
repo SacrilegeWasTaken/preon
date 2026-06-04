cf5d82aa30154245cdabdf4fb179ba40e51b8022
Leonid <superdjskater@mail.ru>
2026-06-04 18:39:55 +0700

    workspace(refactor)/move-asm-away-from-rust-sources-v2

--- kernel/core/asm/boot.s
+++ kernel/core/asm/boot.s
@@ -1,15 +1,15 @@
 .section .text.boot
 .global _start
 _start:
-    ; change the FP/SIMD flag at Coprocessor Access Control Register
+    // change the FP/SIMD flag at Coprocessor Access Control Register
     mrs     x1, cpacr_el1
     orr     x1, x1, #(0x3 << 20)
     msr     cpacr_el1, x1
-    isb ; Flush the CPU pipeline so FP/SIMD instructions coming after wont suddenly fail
-    ; set the stack pointer
+    isb // flush the CPU pipeline so FP/SIMD instructions coming after wont suddenly fail
+    // set the stack pointer
     ldr     x1, =__stack_top
     mov     sp, x1
-    ; load .bss bounds and zero it in a loop
+    // load .bss bounds and zero it in a loop
     ldr     x1, =__bss_start
     ldr     x2, =__bss_end
 1:
@@ -18,7 +18,7 @@ _start:
     str     xzr, [x1], #8
     b       1b
 2:
-    bl      kmain ; jump to rust
+    bl      kmain // jump to rust
 3:
     wfe
     b       3b
